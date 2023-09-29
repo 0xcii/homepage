0c2392da077a24d9b56ca1f049f747a9ef9335d2
0xcii <antcave.club@gmail.com>
2023-09-30 00:00:22 +0800

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -42,7 +42,7 @@ const App: React.FC<AppProps> = (props) => {
     { label: '经典案例', onClick: () => push('/projects') },
     { label: '服务咨询', onClick: () => push('https://t.me/madaodev') },
     { label: '关于我们', onClick: () => push('/CCDAO.pdf') },
-    { label: 'ICEFROG冰蛙', onClick: () => push('https://twitter.com/Ice_Frog666666') },
+    { label: 'Airdrop+', onClick: () => push('https://airdrops.plus') },
   ]
   const mobileMenuItems = [
     { label: '首页', onClick: () => push('/') },
@@ -51,7 +51,7 @@ const App: React.FC<AppProps> = (props) => {
     { label: '经典案例', onClick: () => push('/projects') },
     { label: '服务咨询', onClick: () => push('https://t.me/madaodev') },
     { label: '关于我们', onClick: () => push('/CCDAO.pdf') },
-    { label: 'ICEFROG冰蛙', onClick: () => push('https://twitter.com/Ice_Frog666666') },
+    { label: 'Airdrop+', onClick: () => push('https://airdrops.plus') },
     // { label: '关于', onClick: () => push('/faq') },
   ]
 
@@ -108,7 +108,7 @@ const App: React.FC<AppProps> = (props) => {
           }}
         />
       </Head>
-      <ThemeProvider>
+      {/* <ThemeProvider> */}
         <Web3ReactProvider getLibrary={getLibrary}>
           <Navigation
             menu={menuItems}
@@ -119,7 +119,7 @@ const App: React.FC<AppProps> = (props) => {
           />
           <Component {...pageProps} />
         </Web3ReactProvider>
-      </ThemeProvider>
+      {/* </ThemeProvider> */}
     </>
   )
 }
